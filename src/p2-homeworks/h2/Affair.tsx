5cc6e461d1bda6d77cdcb31fc185ffa19c1b3a9e
Andrey Kulish <andreykulish89@gmail.com>
2022-07-26 14:42:33 +0300

    add: solution of HW2

--- src/p2-homeworks/h2/Affair.tsx
+++ src/p2-homeworks/h2/Affair.tsx
@@ -1,21 +1,39 @@
-import React from 'react'
+import React from "react";
+import { AffairType } from "./HW2";
+import styles from "./Affairs.module.css";
+import cross from "./cross.svg";
 
 type AffairPropsType = {
-    // key не нужно типизировать
-    affair: any // need to fix any
-    deleteAffairCallback: any // need to fix any
-}
+  affair: AffairType; // need to fix any
+  deleteAffairCallback: (_id: number) => void; // need to fix any
+};
 
 function Affair(props: AffairPropsType) {
-    const deleteCallback = () => {}// need to fix
+  const deleteCallback = () => {
+    props.deleteAffairCallback(props.affair._id);
+  };
 
-    return (
-        <div>
-            // show some text
-
-            <button onClick={deleteCallback}>X</button>
+  return (
+    <div className={styles.task}>
+      <div className={styles.task_content}>
+        <div className={styles.task_title}>{props.affair.name}</div>
+        <div
+          className={`${styles.task_priority} ${
+            props.affair.priority === "high"
+              ? styles.task_priorityRed
+              : props.affair.priority === "middle"
+              ? styles.task_priorityYellow
+              : styles.task_priorityGreen
+          }`}
+        >
+          {props.affair.priority}
         </div>
-    )
+      </div>
+      <button className={styles.task_btn} onClick={deleteCallback}>
+        <img src={cross} alt="delete button" />
+      </button>
+    </div>
+  );
 }
 
-export default Affair
+export default Affair;
