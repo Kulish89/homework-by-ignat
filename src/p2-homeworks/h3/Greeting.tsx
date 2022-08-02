977f6f92b2e911b17e4cd8cc3446d6188cf5a532
Andrey Kulish <andreykulish89@gmail.com>
2022-08-02 15:46:38 +0300

    add: solution of HW3

--- src/p2-homeworks/h3/Greeting.tsx
+++ src/p2-homeworks/h3/Greeting.tsx
@@ -1,28 +1,44 @@
-import React from 'react'
-import s from './Greeting.module.css'
+import React from "react";
+import s from "./Greeting.module.css";
 
 type GreetingPropsType = {
-    name: any // need to fix any
-    setNameCallback: any // need to fix any
-    addUser: any // need to fix any
-    error: any // need to fix any
-    totalUsers: any // need to fix any
-}
+  name: string;
+  setNameCallback: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  addUser: () => void;
+  onKeyPressHandler: (e: React.KeyboardEvent<HTMLElement>) => void;
+  error: string;
+  totalUsers: number;
+};
 
-// презентационная компонента (для верстальщика)
-const Greeting: React.FC<GreetingPropsType> = (
-    {name, setNameCallback, addUser, error, totalUsers} // деструктуризация пропсов
-) => {
-    const inputClass = s.error // need to fix with (?:)
+const Greeting: React.FC<GreetingPropsType> = ({
+  name,
+  setNameCallback,
+  addUser,
+  error,
+  totalUsers,
+  onKeyPressHandler,
+}) => {
+  const inputClass = error ? s.error_input : s.someClass;
 
-    return (
-        <div>
-            <input value={name} onChange={setNameCallback} className={inputClass}/>
-            <span>{error}</span>
-            <button onClick={addUser}>add</button>
-            <span>{totalUsers}</span>
-        </div>
-    )
-}
+  return (
+    <div className={s.input_container}>
+      <input
+        value={name}
+        onChange={setNameCallback}
+        className={inputClass}
+        onKeyPress={onKeyPressHandler}
+      />
+      <div className={s.error_message}>{error}</div>
+      <button
+        onClick={addUser}
+        disabled={error ? true : false}
+        className={s.button}
+      >
+        add
+      </button>
+      <span>{totalUsers}</span>
+    </div>
+  );
+};
 
-export default Greeting
+export default Greeting;
--- src/p2-homeworks/h3/GreetingContainer.tsx
+++ src/p2-homeworks/h3/GreetingContainer.tsx
@@ -1,38 +1,55 @@
-import React, {useState} from 'react'
-import Greeting from './Greeting'
+import React, { ChangeEvent, useState } from "react";
+import Greeting from "./Greeting";
+import { UserType } from "./HW3";
 
 type GreetingContainerPropsType = {
-    users: any // need to fix any
-    addUserCallback: any // need to fix any
-}
+  users: Array<UserType>;
+  addUserCallback: (name: string) => void;
+};
 
-// более простой и понятный для новичков
-// function GreetingContainer(props: GreetingPropsType) {
+const GreetingContainer: React.FC<GreetingContainerPropsType> = ({
+  users,
+  addUserCallback,
+}) => {
+  const [name, setName] = useState<string>("");
+  const [error, setError] = useState<string>("");
 
-// более современный и удобный для про :)
-// уровень локальной логики
-const GreetingContainer: React.FC<GreetingContainerPropsType> = ({users, addUserCallback}) => { // деструктуризация пропсов
-    const [name, setName] = useState<any>('') // need to fix any
-    const [error, setError] = useState<any>('') // need to fix any
-
-    const setNameCallback = (e: any) => { // need to fix any
-        setName('') // need to fix
+  const setNameCallback = (e: ChangeEvent<HTMLInputElement>) => {
+    setName(e.currentTarget.value);
+    setError("");
+  };
+  const addUser = () => {
+    if (name) {
+      alert(`Hello ${name}!`);
+      addUserCallback(name);
+      setName("");
+    } else {
+      setError("Please write your name!");
     }
-    const addUser = () => {
-        alert(`Hello  !`) // need to fix
+  };
+  const onKeyPressHandler = (e: React.KeyboardEvent<HTMLElement>) => {
+    if (e.charCode === 13) {
+      if (name) {
+        alert(`Hello ${name}!`);
+        addUserCallback(name);
+        setName("");
+      } else {
+        setError("Please write your name!");
+      }
     }
+  };
+  const totalUsers = users.length;
 
-    const totalUsers = 0 // need to fix
+  return (
+    <Greeting
+      name={name}
+      setNameCallback={setNameCallback}
+      addUser={addUser}
+      onKeyPressHandler={onKeyPressHandler}
+      error={error}
+      totalUsers={totalUsers}
+    />
+  );
+};
 
-    return (
-        <Greeting
-            name={name}
-            setNameCallback={setNameCallback}
-            addUser={addUser}
-            error={error}
-            totalUsers={totalUsers}
-        />
-    )
-}
-
-export default GreetingContainer
+export default GreetingContainer;
