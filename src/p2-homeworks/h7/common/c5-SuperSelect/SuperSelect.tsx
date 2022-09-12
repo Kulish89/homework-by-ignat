213dd22b90b98f97193ff61b16ccdf5d2689ede7
Andrey Kulish <andreykulish89@gmail.com>
2022-09-12 10:56:54 +0300

    HW7

--- src/p2-homeworks/h7/common/c5-SuperSelect/SuperSelect.tsx
+++ src/p2-homeworks/h7/common/c5-SuperSelect/SuperSelect.tsx
@@ -1,30 +1,43 @@
-import React, {SelectHTMLAttributes, DetailedHTMLProps, ChangeEvent} from 'react'
+import React, {
+  SelectHTMLAttributes,
+  DetailedHTMLProps,
+  ChangeEvent,
+} from "react";
 
-type DefaultSelectPropsType = DetailedHTMLProps<SelectHTMLAttributes<HTMLSelectElement>, HTMLSelectElement>
+type DefaultSelectPropsType = DetailedHTMLProps<
+  SelectHTMLAttributes<HTMLSelectElement>,
+  HTMLSelectElement
+>;
 
 type SuperSelectPropsType = DefaultSelectPropsType & {
-    options?: any[]
-    onChangeOption?: (option: any) => void
-}
+  options: Array<string>;
+  onChangeOption: (option: any) => void;
+};
 
-const SuperSelect: React.FC<SuperSelectPropsType> = (
-    {
-        options,
-        onChange, onChangeOption,
-        ...restProps
-    }
-) => {
-    const mappedOptions: any[] = []; // map options with key
-
-    const onChangeCallback = (e: ChangeEvent<HTMLSelectElement>) => {
-        // onChange, onChangeOption
-    }
-
+const SuperSelect: React.FC<SuperSelectPropsType> = ({
+  options,
+  onChange,
+  onChangeOption,
+  ...restProps
+}) => {
+  const mappedOptions: any[] = options.map((op, index) => {
     return (
-        <select onChange={onChangeCallback} {...restProps}>
-            {mappedOptions}
-        </select>
-    )
-}
+      <option key={index} value={op}>
+        {op}
+      </option>
+    );
+  }); // map options with key
 
-export default SuperSelect
+  const onChangeCallback = (e: ChangeEvent<HTMLSelectElement>) => {
+    onChangeOption && onChangeOption(e.currentTarget.value);
+    onChange && onChange(e);
+  };
+
+  return (
+    <select onChange={onChangeCallback} {...restProps}>
+      {mappedOptions}
+    </select>
+  );
+};
+
+export default SuperSelect;
